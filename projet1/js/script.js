da75a22752efb99ccfbacea5dbe8f64bf25e9791
mo_aidara <mansouraidara43@gmail.com>
2024-10-28 17:08:50 +0100

    enregistrement des modification sur la page index (js,style)

--- projet1/js/script.js
+++ projet1/js/script.js
@@ -1,30 +1,48 @@
+function ok(){
+    document.getElementById('dialog-box').classList.remove('d-flex');
+    document.getElementById('dialog-box').classList.add('d-none');
+}
+
 document.addEventListener('DOMContentLoaded', function(){
     
     
     
-    let nombre_secret = 5;
-
+    let nombre_secret = Math.random().toFixed(2) *100;
+    let essaie=1;
     document.getElementById('valider').addEventListener('click', function(){
      
         let variable; 
-        let essaie=1;
-        essaie++
+        document.getElementById('reessayer').disabled = true;
+        
         variable = parseInt(document.getElementById("nombre").value);
     
         if(variable > nombre_secret){
-            alert("le nombre que vous avez choisit est plus grande!");
+            document.getElementById('dialog-box').classList.remove('d-none');
+            document.getElementById('dialog-box').classList.add('d-flex');
+            document.getElementById('indication').innerHTML = "Le nombre choisie est superieur!"; 
+            document.getElementById('Attention').innerHTML = "il ne vous reste que " + (5-essaie) + (" essai");
         }else if(variable === nombre_secret){
-            alert("le nombre que vous avez choisit est egal! vous avez gagniez");
-            
+            document.getElementById('dialog-box').classList.remove('d-none');
+            document.getElementById('dialog-box').classList.add('d-flex');
+            document.getElementById('indication').innerHTML = "vous avez gagniez";
+            document.getElementById('valider').disabled = true;
         }else if(variable < nombre_secret){
-            alert("le nombre que vous avez choisit est plus petit!");
+            document.getElementById('dialog-box').classList.remove('d-none');
+            document.getElementById('dialog-box').classList.add('d-flex');
+            document.getElementById('indication').innerHTML = "Le nombre choisie est inferieur!";
+            document.getElementById('Attention').innerHTML = "il ne vous reste que " + (5-essaie) + (" essai");
         }  
-
+        essaie++
         if(essaie>5){
             document.getElementById('valider').disabled = true;
+            document.getElementById('reessayer').disabled = false;
         }
+        document.getElementById('reessayer').addEventListener('click', function(){
+            location.reload();
+        });
         
     });
+    console.log(nombre_secret);
 
 })
 
